5f84fe0eeccf8b6d103471146713b2cb042de690
iTamojeet <bigcodingdinosaur@gmail.com>
2025-08-23 01:25:00 +0530

    almost final

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 // Staggered cards
-const staggerCards = document.querySelectorAll('.stagger-fade');
-const cardObserver = new IntersectionObserver(entries => {
-  entries.forEach((entry, i) => {
-    if (entry.isIntersecting) {
-      setTimeout(() => entry.target.classList.add('visible'), i * 150);
-      cardObserver.unobserve(entry.target);
-    }
-  });
-}, { threshold: 0.2 });
+const staggerCards = document.querySelectorAll(".stagger-fade");
+const cardObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry, i) => {
+      if (entry.isIntersecting) {
+        setTimeout(() => entry.target.classList.add("visible"), i * 150);
+        cardObserver.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
 
-staggerCards.forEach(card => cardObserver.observe(card));
+staggerCards.forEach((card) => cardObserver.observe(card));
 
 // typed-text
-const typedText = document.getElementById('typed-text');
+const typedText = document.getElementById("typed-text");
 const options = {
   strings: [
-    "CEO and Founder of MakeCodeEasy",
+    "Founder and CEO of Make Code Easy",
     "Bronze Medalist of IIT Bombay Campus Ambassador",
-    "Passionate Educator & Tech Mentor",
+    "Face of Internshala",
+    "CA and Face of Physics Wallah",
+    "Passionate Educator and Tech Mentor",
     "Bridging Classroom to Career Gap",
   ],
-  typeSpeed: 50,
+  typeSpeed: 30,
   backSpeed: 25,
-  loop: true
+  loop: true,
 };
 const typed = new Typed(typedText, options);
